1f4c089cb8cb0b333289e439bf602507234d2cc1
lizbur10 <liz.burton147@gmail.com>
2018-02-17 19:28:56 -0500

    minor tweaks

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -49,7 +49,7 @@ class BirdsController < ApplicationController
                 report.save
                 params[:bird][:number_banded].to_i.times do
                     bird = Bird.new(:banding_date => params[:date])
-                    bird.species = find_species_by_code || bird.species = Helpers.create_species(params[:bird][:species])
+                    bird.species = find_species_by_code || Helpers.create_species(params[:bird][:species])
                     bird.bander = Helpers.current_bander(session)
                     bird.save
                 end
